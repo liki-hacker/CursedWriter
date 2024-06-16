f3dd1cfd69672ff27642d6341314acd23f075371
Likhita Dangeti <122101271+liki-hacker@users.noreply.github.com>
2024-06-16 15:06:45 +0530

    switch position change

--- src/template.js
+++ src/template.js
@@ -6,9 +6,9 @@ checkbox.addEventListener("change", (e) => {
     : (document.body.style.cssText = `background: #FFF5D9`);
 });
 
-const instadir = document.getElementById("text2");
+/*const instadir = document.getElementById("text2");
   instadir.addEventListener("click", () => {
     //tl.restart();
     window.location.href = "https://www.instagram.com/urs_truelie/"
   });
-};
+};*/
