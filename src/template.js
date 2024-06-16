74cb2b9efaa1b3e109bd58efd0adde35d291800c
Likhita Dangeti <122101271+liki-hacker@users.noreply.github.com>
2024-06-16 14:33:37 +0530

    Update template.js

--- src/template.js
+++ src/template.js
@@ -5,3 +5,10 @@ checkbox.addEventListener("change", (e) => {
     ? (document.body.style.cssText = `background: #01131F`)
     : (document.body.style.cssText = `background: #FFF5D9`);
 });
+
+const instadir = document.getElementById("text2");
+  instadir.addEventListener("click", () => {
+    //tl.restart();
+    window.location.href = "https://www.instagram.com/urs_truelie/"
+  });
+};
